39539de38413882eb2cebf82f6186d9e0c910c01
lahcene <lahcene.haid@gmail.com>
2026-02-17 17:17:29 +0100

    add const card & .map

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,51 @@ import Card from './components/card'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cards = [
+  {
+    id: 1,
+    title: "Bas Jogging Stone island",
+    description: "Bas de jogging 100% cotton",
+    image: "https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQBcVcFSyayKH0ptNIURXdOnf71J4GMZqe8WhP"
+  },
+  {
+    id: 2,
+    title: "Veste sans manches Stone island",
+    description: "Veste sans manches technique",
+    image: "https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQbHmsk672dVrRhKQaPyYNnjz0lX1gDk4EiMLB"
+  },
+  {
+    id: 3,
+    title: "Sweat a capuche Stone island",
+    description: "Sweat a capuche 100% cotton",
+    image: "https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQLjZvxoeC0T1gHoQhJbsv2jYGxyFcNP8Vki4p"
+  },
+  {
+    id: 4,
+    title: "Short Stone island",
+    description: "Short 100% cotton",
+    image: "https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQcPkvsQz910uoaXvOQSWrmPnTAzhs28xNb5Kd"
+  },
+  {
+    id: 5,
+    title: "Sweat a capuche Stone island",
+    description: "Sweat a capuche cotton",
+    image: "https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQkoNhM3KqDN6LWgK1FOxP7U9Z43IB5VqYyCMk"
+  },
+    {
+    id: 6,
+    title: "Sweat a capuche Stone island",
+    description: "Sweat a capuche cotton",
+    image: "https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQi4KiGGnpRmuY4nU08zqbDsJtTe5c91VvABPg"
+  },
+   {
+    id: 7,
+    title: "T-shirt Dior Oblique",
+    description: "Tshirt avec signe oblique",
+    image: "https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQLIU4v0eC0T1gHoQhJbsv2jYGxyFcNP8Vki4p"
+  },
+
+];
 
   return (
     <>
@@ -19,13 +63,17 @@ function App() {
     <h2 className="namelist">My Dressing</h2>
     
     <main>
-      <Card 
-      image="https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQBcVcFSyayKH0ptNIURXdOnf71J4GMZqe8WhP"
-      description="Ceci est la première carte" />
-      <Card 
-      image="https://hh84n94hcl.ufs.sh/f/GM7nKIBBdnSQbHmsk672dVrRhKQaPyYNnjz0lX1gDk4EiMLB"
-      description="Voici une deuxième carte"
-      />
+      <main>
+      {cards.map(card => (
+        <Card
+        key={card.id}            
+        title={card.title}        
+        description={card.description}  
+        image={card.image}        
+    />
+  ))}
+</main>
+
     </main>
 
     <footer>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,11 @@
-export default Card;
-function Card({ image, description }) {
+function Card({ title, image, description }) {
   return (
     <div className="card">
-      <img src={image} alt="illustration" />
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
       <p>{description}</p>
     </div>
   );
 }
+
+export default Card;
